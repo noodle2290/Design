cbbfd58d7c5077479f1289c3264d9afa84cd0db5
takuro <67560179+nuaseTakuro@users.noreply.github.com>
2021-07-23 21:51:27 +0900

    完成

--- app/src/main/java/app/murakami/takuro/youtubedesign/MainActivity.kt
+++ app/src/main/java/app/murakami/takuro/youtubedesign/MainActivity.kt
@@ -2,15 +2,17 @@ package app.murakami.takuro.youtubedesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val movieData: List<Moviedata> = listOf(
-        Moviedata(R.drawable.vlog1,R.drawable.randy,"東北一週VLOG","ランディー・1700回"),
-        Moviedata(R.drawable.tech,R.drawable.randy,"[Life is Tech]Leaders研修","ランディー・1400回"),
-        Moviedata(R.drawable.vlog2,R.drawable.randy,"四国一周VLOG","ランディー・100回"),
+        Moviedata(R.drawable.vlog1,R.drawable.randy,"東北一週VLOG","ランディー・1700回視聴・3日前"),
+        Moviedata(R.drawable.tech,R.drawable.randy,"[Life is Tech]Leaders研修","ランディー・1400回視聴・5日前"),
+        Moviedata(R.drawable.vlog2,R.drawable.randy,"四国一周VLOG","ランディー・100回視聴・1ヵ月前"),
         Moviedata(R.drawable.vlog1,R.drawable.randy,"東北一週VLOG","ランディー・1700回"),
         Moviedata(R.drawable.tech,R.drawable.randy,"[Life is Tech]Leaders研修","ランディー・1400回"),
         Moviedata(R.drawable.vlog2,R.drawable.randy,"四国一周VLOG","ランディー・100回"),
@@ -36,5 +38,6 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
 
         adapter.addAll(movieData)
+
     }
 }
